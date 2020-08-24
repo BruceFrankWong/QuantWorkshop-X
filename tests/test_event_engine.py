10b7d2957ee6e21083ecc57fdcdffda5d33aaaab
Bruce Frank Wong <github+bruce.frank.wong@gmail.com>
2020-08-24 16:26:15 +0800

    Add some tests.

--- tests/test_event_engine.py
+++ tests/test_event_engine.py
@@ -7,15 +7,21 @@ from QuantWorkshop.engine import QWEventEngine
 from QuantWorkshop.types import QWEventType, QWEvent
 
 
+# TODO: How to test a event loop?
 class QWMultiThreadingEventEngineTestCase(unittest.TestCase):
+    timer_callback_counter: int
+
     def setUp(self) -> None:
         self.event_engine = QWEventEngine()
+        self.timer_callback_counter = 0
 
     def tearDown(self) -> None:
         self.event_engine.stop()
 
     def timer_callback(self, event: QWEvent):
-        self. assertEqual('TimerTriggered', event.data)
+        self.assertEqual('TimerTriggered', event.data)
+        self.timer_callback_counter += 1
+        self.assertEqual(1, self.timer_callback_counter, 'timer_callback called more than one times.')
 
     def test_timer_should_callback(self):
         self.event_engine.register(QWEventType.TimerEvent, self.timer_callback)
